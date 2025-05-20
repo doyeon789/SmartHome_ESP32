172a4d0f21bc52cb94d7b8a2ea5fb27d81aaf67b
doyeon789 <143865473+doyeon789@users.noreply.github.com>
2025-05-20 16:09:02 +0900

    Update main.c

--- esp32UARTConnect/esp32UARTConnect/main.c
+++ esp32UARTConnect/esp32UARTConnect/main.c
@@ -1,16 +1,12 @@
-/*
- * esp32UARTConnect.c
- *
- * Created: 2025-04-16 오후 12:22:13
- * Author : user
- */ 
-#define F_CPU 16000000UL  // Ensure that the CPU frequency is correct
+#define F_CPU 16000000UL
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
 #define BAUD 9600
-#define MY_UBRR F_CPU/16/BAUD-1  // Baud rate calculation for 9600
+#define MY_UBRR F_CPU/16/BAUD-1
+
+#define STR_SIZE 20  // ✅ 문자열 버퍼 크기
 
 void usart0_init(unsigned int UBRR0){
 	UBRR0H = (unsigned char)(UBRR0>>8);
@@ -19,36 +15,39 @@ void usart0_init(unsigned int UBRR0){
 }
 
 void tx0_ch(unsigned char data){
-	while(!(UCSR0A & (1<<UDRE0)));  // Wait until buffer is empty
-	UDR0 = data;  // Transmit data
+	while(!(UCSR0A & (1<<UDRE0)));
+	UDR0 = data;
 }
 
 void tx0_str(unsigned char *str){
-	while(*str){  // Send each character of the string until '\0'
+	while(*str){
 		tx0_ch(*str++);
 	}
 }
 
 char rx0_ch(){
-	while(!(UCSR0A & (1<<RXC0)));  // Wait for data to be received
-	return UDR0;  // Return received character
+	while(!(UCSR0A & (1<<RXC0)));
+	return UDR0;
 }
 
 int main(void) {
-	DDRA = 0xff;
-	
-	char str[20] = "";  // Initialize empty string buffer
-	unsigned char idx = 0;  // Index to track position in the string
+	DDRA = 0xFF;
 
-	usart0_init(MY_UBRR);  // Initialize UART
+	char str[STR_SIZE] = "";  // ✅ STR_SIZE 사용
+	unsigned char idx = 0;
+
+	usart0_init(MY_UBRR);
 
 	while (1) {
-		char received = rx0_ch();  // Read received character
-		if (received != '\0' && idx < 19) {  // Ensure space for '\0'
-			str[idx++] = received;  // Store character
-			str[idx] = '\0';  // Null-terminate string
-			tx0_str(str);
+		char received = rx0_ch();
+		if (received == '\n' || received == '\r') {
+			str[idx] = '\0';     // 끝 표시
+			tx0_str(str);        // 문자열 출력
 			tx0_str("\n");
+			idx = 0;             // 버퍼 초기화
+		}
+		else if (idx < STR_SIZE - 1) {
+			str[idx++] = received;
 		}
 	}
 }
