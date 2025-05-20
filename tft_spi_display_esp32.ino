bfe30eb6be78247b99df08d0d2626568eaeb0ad8
doyeon789 <143865473+doyeon789@users.noreply.github.com>
2025-05-20 15:31:24 +0900

    Update tft_spi_display_esp32.ino

--- tft_spi_display_esp32.ino
+++ tft_spi_display_esp32.ino
@@ -1,38 +1,9 @@
-// 320x240
-#include <Arduino_GFX_Library.h>
-
-#define CS    15
-#define RESET 4
-#define DC    2
-#define SDI   23
-#define SCK   18
-
-void setup() {
-  Arduino_DataBus *bus = new Arduino_HWSPI(DC , CS);
-  Arduino_GFX *display = new Arduino_ILI9341(bus,RESET);
-
-  display->begin();
-  display->setRotation(3);
-  display->fillScreen(RGB565_BLACK);
-  display->setCursor(10, 10);
-  display->setTextColor(RGB565_WHITE);
-  display->setTextSize(5);
-  display->println("Hello\nWorld!");
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-} 
-
-
-/* 480x320
 #include <Arduino_GFX_Library.h>
 
 #define TFT_SCK    18
 #define TFT_MOSI   23
 #define TFT_MISO   19
-#define TFT_CS     15
+#define TFT_CS     5
 #define TFT_DC      2
 #define TFT_RESET   4
 
@@ -60,5 +31,3 @@ void setup(void)
 }
 
 void loop() {}
-
-*/
