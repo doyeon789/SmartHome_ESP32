87345c29d7c9f16eebebfdefd69a6cbbf92266eb
doyeon789 <143865473+doyeon789@users.noreply.github.com>
2025-05-26 18:36:53 +0900

    Update main.c

--- LEDON/LEDON/main.c
+++ LEDON/LEDON/main.c
@@ -8,6 +8,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <string.h>
 
 void timer1_pwm_init(){
 	DDRB|=(1<<PB5)|(1<<PB6)|(1<<PB7);
@@ -24,19 +25,71 @@ ISR(TIMER1_COMPA_vect){}
 ISR(TIMER1_COMPB_vect){}
 ISR(TIMER1_COMPC_vect){}
 	
+	
+void usart0_init(unsigned int UBRR0){
+	UBRR0H = (unsigned char)(UBRR0>>8);
+	UBRR0L = (unsigned char)UBRR0;
+	UCSR0B = (1<<TXEN0)|(1<<RXEN0);
+}
+
+void tx0_ch(unsigned char data){
+	while(!(UCSR0A & (1<<UDRE0)));
+	UDR0 = data;
+}
+
+void tx0_str(unsigned char *str){
+	while(*str){
+		tx0_ch(*str++);
+	}
+}
+
+char rx0_ch(){
+	while(!(UCSR0A & (1<<RXC0)));
+	return UDR0;
+}
+
 int main(void) {
 	DDRA = 0xff;
 	DDRB = 0xff;
 	
+	char str[20] = "";  // ✅ STR_SIZE 사용
+	unsigned char idx = 0;
+	int values[20];  // 숫자 저장 배열
+	
+	usart0_init(103);
 	timer1_pwm_init();
 
     /* Replace with your application code */
-    while (1)  {
-		PORTA= 0xff;
-		PORTB = (1<<PB0)|(1<<PB1)|(1<<PB2);
-		
+
+	while (1) {
+		char received = rx0_ch();
+		if (received == '\n' || received == '\r') {
+			str[idx] = '\0';     // 끝 표시
+			tx0_str(str);        // 문자열 출력
+			tx0_str("\n");
+			
+			for(int i=0;i<8;i++){
+				if(values[i] == 0)
+				PORTA &= ~(1<<i);
+				else
+				PORTA |= (1<<i);
+			}
+			for(int j=0;j<=3;j++){
+				if(values[j+8] == 0)
+				PORTB &= ~(1<<j);
+				else
+				PORTB |= (1<<j);
+			}
+			idx = 0;
+		}
+		else if (idx < 20 - 1) {
+			str[idx] = received;
+			values[idx++] = atoi(received);
+		}
+	
 		OCR1A = 0xff; //R
 		OCR1B = 0xff; //G
 		OCR1C = 0xff; //B
-    }
+	}
+
 }
