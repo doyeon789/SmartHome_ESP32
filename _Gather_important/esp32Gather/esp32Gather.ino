7a574962da254294dca1631562effb42a7e6a23f
doyeon789 <143865473+doyeon789@users.noreply.github.com>
2025-06-24 04:08:47 +0900

    Update esp32Gather.ino

--- _Gather_important/esp32Gather/esp32Gather.ino
+++ _Gather_important/esp32Gather/esp32Gather.ino
@@ -6,10 +6,10 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 
-DHT dht(16, DHT11);
-
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+DHT dht(16, DHT11);
 
 BLEServer* pServer = nullptr;
 BLECharacteristic* pCharacteristic = nullptr;
